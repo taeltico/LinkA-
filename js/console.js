e4706e4b8afd3544c7adcae2b63f342bd3313958
Rafael Wanderley <Rafaelwvs16@icloud.com>
2022-10-11 22:18:00 -0300

    node

---
+++ js/console.js
@@ -0,0 +1,11 @@
+let Rafael = 1991;
+
+console.log(Rafael);
+
+const combustivel = 5.04;
+let KmPorLitro = 12.90;
+const distancia_percorrida_em_km = 208;
+
+let gasto_de_combustivel = (distancia_percorrida_em_km / KmPorLitro)*combustivel;
+
+console.log(gasto_de_combustivel.toFixed(2));
