969ff9b8dc959f1446ada27891e9e9c9cbd2ea36
Rafael <rafaelwvs16@icloud.com>
2022-10-11 22:33:48 -0300

    config email

--- js/console.js
+++ js/console.js
@@ -7,8 +7,11 @@ let KmPorLitro = 12.90;
 const distancia_percorrida_em_km = 208;
 
 let gasto_de_combustivel = (distancia_percorrida_em_km / KmPorLitro)*combustivel;
-let gasto_na_em_valor = (distancia_percorrida_em_km * combustivel)
+let gasto_na_em_valor = (distancia_percorrida_em_km * combustivel);
+let litros_no_percurso = (distancia_percorrida_em_km / KmPorLitro);
 
 console.log(gasto_de_combustivel.toFixed(2));
-console.log(gasto_na_em_valor.toFixed(3))
+console.log(gasto_na_em_valor.toFixed(2));
+console.log(litros_no_percurso.toFixed(2));
+
 
