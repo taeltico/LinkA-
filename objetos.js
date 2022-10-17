853fc55e9b89e57e9e79659955b0bebb2e036345
Rafael <Rafaelwvs16@gmail.com>
2022-10-17 17:14:37 -0300

    objetos

---
+++ objetos.js
@@ -0,0 +1,19 @@
+const rafael = {
+    nome:'Rafael Wanderley Vitor Sá',
+    idade: 31,
+    Sexo: 'masc',
+
+    apresentacao: function() {
+        console.log('meu nome é  ${this.nome}  e minha idade é ${this.idade} minha profissão é  ${this.formacao}');
+    }
+};
+
+console.log(rafael.nome);
+console.log(rafael.sexo);
+console.log(rafael);
+
+rafael.alt = 1.75;
+rafael.formacao = 'programador';
+
+console.log(rafael)
+
